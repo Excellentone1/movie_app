ec24298ec272dc49c813f320e2d01bc26b9bca63
Excellentone1 <excellentone177@gmail.com>
2022-11-06 16:20:33 +0300

    Updates

---
+++ precache-manifest.cd45ee5808dcd8def9386c49be6cd843.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f6f988f521decf76d15",
+    "url": "/movie_app/static/css/main.8ca8f41b.chunk.css"
+  },
+  {
+    "revision": "4f6f988f521decf76d15",
+    "url": "/movie_app/static/js/main.4f6f988f.chunk.js"
+  },
+  {
+    "revision": "d09f27f71a1539448e12",
+    "url": "/movie_app/static/js/1.d09f27f7.chunk.js"
+  },
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "5fc6ab5b1adf7fbeb2f1237218134702",
+    "url": "/movie_app/index.html"
+  }
+];
